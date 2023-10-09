a95456645172c9091ff4cc39ebac3b1b046f694a
nicolas sage <nicolas.sage@neoteem.fr>
2023-10-09 17:47:29 +0200

    ok

--- src/gMethods.js
+++ src/gMethods.js
@@ -3,20 +3,38 @@ import { gVariables } from "./gVariables";
 
 // --------- Methodes gloables ---------
 export const gMethods = {
-  products: {
-    createProduct() {
-      let body = {
-        name: "From front",
-        prix: 9,
-      };
-      axios
-        .post(`${gVariables.config.domain}/product`, body)
-        .then((response) => {
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.error("Error fetching products data:", error);
-        });
+    products: {
+        createProduct() {
+            let body = {
+                name: "From front",
+                prix: 9,
+            };
+            axios
+                .post(`${gVariables.config.domain}/product`, body)
+                .then((response) => {
+                    console.log(response.data);
+                })
+                .catch((error) => {
+                    console.error("Error fetching products data:", error);
+                });
+        },
     },
-  },
+    users: {
+        createUser(name, adress, email, password) {
+            let body = {
+                name: name,
+                adress: adress,
+                email: email,
+                password: password,
+            };
+            axios
+                .post(`${gVariables.config.domain}/user`, body)
+                .then((response) => {
+                    console.log(response.data);
+                })
+                .catch((error) => {
+                    console.error("Error fetching products data:", error);
+                });
+        },
+    }
 };
--- src/views/User/Register.vue
+++ src/views/User/Register.vue
@@ -1,14 +1,73 @@
 <template>
-  <div class="register">
-    a
-    <v-text-field label="Label"></v-text-field>
-  </div>
+    <div class="register ma-4">
+        <h2>S'inscrire</h2>
+
+        <div class="formulaire mt-2 w-50">
+            <v-text-field
+                v-model="user.name"
+                class="mb-2"
+                variant="outlined"
+                density="compact"
+                hide-details="true"
+                label="Nom" />
+
+            <v-text-field
+                v-model="user.firstname"
+                class="mb-2"
+                variant="outlined"
+                density="compact"
+                hide-details="true"
+                label="Prénom" />
+
+            <v-text-field
+                v-model="user.adress"
+                class="mb-2"
+                variant="outlined"
+                density="compact"
+                hide-details="true"
+                label="Adresse" />
+
+            <v-text-field
+                v-model="user.email"
+                class="mb-2"
+                variant="outlined"
+                density="compact"
+                hide-details="true"
+                label="Mail" />
+
+            <v-text-field
+                v-model="user.password"
+                class="mb-2"
+                variant="outlined"
+                density="compact"
+                hide-details="true"
+                label="Mot de passe" />
+
+            <v-btn
+                color="blue"
+                variant="tonal"
+                @click="$m.users.createUser(`${user.name} ${user.firstname}`, user.adress, user.email, user.password)">
+                S'inscrire
+            </v-btn>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "register",
-};
+    name: 'register',
+    data() {
+        return {
+            user: {
+                name: '',
+                firstname: '',
+                email: '',
+                password: '',
+                adress: ''
+            }
+        }
+    }
+}
 </script>
 
 <style lang="scss" scoped></style>
